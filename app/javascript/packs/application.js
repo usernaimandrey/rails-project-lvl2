14e6cd3b50d8b09625cf635ca492188db71931f3
usernaimandrey <anshlyapnikov@yandex.ru>
2022-07-27 00:05:04 +0300

    add_botstarp

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,8 @@
 
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "@fortawesome/fontawesome-free/css/all"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :confirmable, :lockable, :recoverable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :rememberable, :validatable
 end
